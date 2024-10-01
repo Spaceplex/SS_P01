5d173574ccc1e711e89c82f30121b62501af1957
Spaceplex <spaecplex@gmail.com>
2024-10-01 18:38:36 -0400

    Honor code

--- SS_P01.java
+++ SS_P01.java
@@ -1,3 +1,61 @@
+/** 
+
+//*********************************************************************** 
+
+'Project: Assignment 1 
+
+'Programmer: Sohail Samar
+
+'Company Info: ssamar@mymail.csmd.edu   
+
+'Date: 01 10 2024 
+
+'Description:  Problem Number 1.   
+
+'     
+
+'   LINE 1 AT LEAST 3 LINES OF PROGRAM DESCRIPTION 
+
+'   LINE 2 AT LEAST 3 LINES OF PROGRAM DESCRIPTION 
+
+'   LINE 3 AT LEAST 3 LINES OF PROGRAM DESCRIPTION 
+
+' 
+
+'	-------------------------------------------------------------------------- 
+
+'   							HONOR CODE:  
+
+'	I pledge that this program represents my own program code, I have received  
+
+'	help from no one and I have given help to no one. 
+
+'	 
+
+'								OR 
+
+' 
+
+'	I received help from NAME OR NO ONE in designing and debugging my program. 
+
+'	I given help to NAME OR NO ONE in designing and debugging my program. 
+
+'----------------------------------------------------------------------------- 
+
+' 
+
+'  LINE LENGTH - AVOID LINES LONGER THAN 80 CHARACTERS 
+
+'  SCALE BELOW IS TO CALIBRATE SCREENSHOTS 
+
+'  DO NOT HAVE YOUR CODE OR SCREENSHOT EXTEND BEYOND THE SCALE 
+
+0........1.........2.........3.........4.........5.........6.........7.........8 
+
+12345678901234567890123456789012345678901234567890123456789012345678901234567890 
+
+*/ 
+
 import javax.swing.*;
 import java.awt.event.*;
 
